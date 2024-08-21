7ccf78e3cdc0f6eed46a1f6af1cbe2d8f5d4d3c7
G-jjunny <rudwns9551@naver.com>
2024-08-21 17:42:56 +0900

    수정

--- src/components/Consult-components/Map.jsx
+++ src/components/Consult-components/Map.jsx
@@ -17,7 +17,16 @@ const Map = () => {
     });
 
     marker.setMap(map);
+    var iwContent =
+        '<div class="info-window"><div class="info-window-head">우드원</div><div class="info-window-add"></div><div class="info-window-link"><a href="https://map.kakao.com/link/map/강남구 논현로128길 22-4,37.513473, 127.033144" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/강남구 논현로128길 22-4,37.513473, 127.033144" style="color:blue" target="_blank">길찾기</a></div></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+      iwPosition = new kakao.maps.LatLng(37.513473, 127.033144); //인포윈도우 표시 위치입니다
 
+    var infowindow = new kakao.maps.InfoWindow({
+      position: iwPosition,
+      content: iwContent,
+    });
+    // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+    infowindow.open(map, marker);
     map.setZoomable(false);
     map.relayout();
   }, []);
